05da9534848e839ca4224ae897f50cde50f4a0d5
Vinicius Ferreira Negrisolo <vinicius.negrisolo@gmail.com>
2016-09-03 18:13:16 -0400

    Move set to local storage to background

--- background.js
+++ background.js
@@ -1,5 +1,45 @@
+var TITLES = [
+        "Admiral",
+        "Better Call Saul", // waiting new season
+        "Braveheart",
+        "Breaking Bad",
+        "Defiance",
+        "Dexter",
+        "Django Unchained",
+        "Elizabeth: The Golden Age",
+        "Fearless",
+        "Friends",
+        "Gladiator",
+        "Jobs",
+        "House of Cards", // waiting new season
+        "Look Who's Back",
+        "Lost",
+        "Marco Polo", // waiting new season
+        "Marvel's Daredevil", // waiting new season
+        "Marvel's Jessica Jones", // waiting new season
+        "Nightcrawler",
+        "Pirates of the Caribbean: The Curse of the Black Pearl",
+        "Sense8", // waiting new season
+        "Spartacus",
+        "Spotlight",
+        "Stranger Things",
+        "The Big Short",
+        "The Cobbler",
+        "The Interview",
+        "The Last Kingdom",
+        "The Ridiculous 6",
+        "The Truman Show",
+        "The Tudors",
+        "Top Gun",
+        "Turn", // waiting new season
+        "V for Vendetta",
+        "Weeds"
+];
+
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
   if (tab.url.indexOf('https://www.netflix.com/browse') == 0) {
+    chrome.storage.local.set({'titles': TITLES});
+
     chrome.pageAction.show(tabId);
   }
 });
--- content.js
+++ content.js
@@ -1,48 +1,9 @@
-var TITLES = [
-        "Admiral",
-        "Better Call Saul", // waiting new season
-        "Braveheart",
-        "Breaking Bad",
-        "Defiance",
-        "Dexter",
-        "Django Unchained",
-        "Elizabeth: The Golden Age",
-        "Fearless",
-        "Friends",
-        "Gladiator",
-        "Jobs",
-        "House of Cards", // waiting new season
-        "Look Who's Back",
-        "Lost",
-        "Marco Polo", // waiting new season
-        "Marvel's Daredevil", // waiting new season
-        "Marvel's Jessica Jones", // waiting new season
-        "Nightcrawler",
-        "Pirates of the Caribbean: The Curse of the Black Pearl",
-        "Sense8", // waiting new season
-        "Spartacus",
-        "Spotlight",
-        "Stranger Things",
-        "The Big Short",
-        "The Cobbler",
-        "The Interview",
-        "The Last Kingdom",
-        "The Ridiculous 6",
-        "The Truman Show",
-        "The Tudors",
-        "Top Gun",
-        "Turn", // waiting new season
-        "V for Vendetta",
-        "Weeds"
-];
 var SEL_P = '[aria-label="';
 var SEL_S = '"]';
 
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse){
     if(request.message === 'foo'){
-      chrome.storage.local.set({'titles': TITLES}, function() {
-      });
 
       chrome.storage.local.get('titles', function(value) {
         value.titles.forEach(function(x, i, xs){

6cfc8ee8c502a1d8680c7e9cb9dba219633a69cc
Vinicius Ferreira Negrisolo <vinicius.negrisolo@gmail.com>
2016-09-04 15:13:31 -0400

    Change the way popup.js is loaded

--- popup.js
+++ popup.js
@@ -36,24 +36,26 @@ var TITLES = [
   "Weeds"
 ];
 
-document.body.onload = function(){
-  chrome.storage.sync.set({'data': TITLES});
+document.addEventListener('DOMContentLoaded', function() {
+  document.body.onload = function(){
+    chrome.storage.sync.set({'data': TITLES});
 
-  chrome.storage.sync.get('data', function(data){
-    document.getElementById('titles').innerHTML = "";
+    chrome.storage.sync.get('data', function(data){
+      document.getElementById('titles').innerHTML = "";
 
-    data.data.forEach(function(title){
-      var node = document.createElement("li");
-      var text = document.createTextNode(title);
-      node.appendChild(text);
+      data.data.forEach(function(title){
+        var node = document.createElement("li");
+        var text = document.createTextNode(title);
+        node.appendChild(text);
 
-      document.getElementById('titles').appendChild(node);
+        document.getElementById('titles').appendChild(node);
+      });
     });
-  });
-}
+  }
 
-document.getElementById('clear').onclick = function(){
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-    chrome.tabs.sendMessage(tabs[0].id, {'message': 'foo'});
-  });
-};
+  document.getElementById('clear').onclick = function(){
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+      chrome.tabs.sendMessage(tabs[0].id, {'message': 'foo'});
+    });
+  };
+});

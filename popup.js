070cef2e89d343764a1beb20a08cd53b3d122b43
Vinicius Ferreira Negrisolo <vinicius.negrisolo@gmail.com>
2016-09-04 14:10:39 -0400

    Add clear button to popup html

--- popup.js
+++ popup.js
@@ -39,3 +39,9 @@ var TITLES = [
 document.body.onload = function(){
   chrome.storage.sync.set({'data': TITLES});
 }
+
+document.getElementById('clear').onclick = function(){
+  chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+    chrome.tabs.sendMessage(tabs[0].id, {'message': 'foo'});
+  });
+};

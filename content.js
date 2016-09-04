ac98f03a9db64458d8a02007fb5c0eced899eaf6
Vinicius Ferreira Negrisolo <vinicius.negrisolo@gmail.com>
2016-09-04 14:09:09 -0400

    Set Titles in popup.js instead of background

--- content.js
+++ content.js
@@ -5,8 +5,8 @@ chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse){
     if(request.message === 'foo'){
 
-      chrome.storage.local.get('titles', function(value) {
-        value.titles.forEach(function(x, i, xs){
+      chrome.storage.local.get('data', function(data) {
+        data.data.forEach(function(x, i, xs){
           document.querySelectorAll(SEL_P + x + SEL_S).forEach(function(y, j, ys){
             y.parentNode.parentNode.style.display = 'none';
           });
---
+++ popup.js
@@ -0,0 +1,41 @@
+var TITLES = [
+  "Admiral",
+  "Better Call Saul", // waiting new season
+  "Braveheart",
+  "Breaking Bad",
+  "Defiance",
+  "Dexter",
+  "Django Unchained",
+  "Elizabeth: The Golden Age",
+  "Fearless",
+  "Friends",
+  "Gladiator",
+  "Jobs",
+  "House of Cards", // waiting new season
+  "Look Who's Back",
+  "Lost",
+  "Marco Polo", // waiting new season
+  "Marvel's Daredevil", // waiting new season
+  "Marvel's Jessica Jones", // waiting new season
+  "Nightcrawler",
+  "Pirates of the Caribbean: The Curse of the Black Pearl",
+  "Sense8", // waiting new season
+  "Spartacus",
+  "Spotlight",
+  "Stranger Things",
+  "The Big Short",
+  "The Cobbler",
+  "The Interview",
+  "The Last Kingdom",
+  "The Ridiculous 6",
+  "The Truman Show",
+  "The Tudors",
+  "Top Gun",
+  "Turn", // waiting new season
+  "V for Vendetta",
+  "Weeds"
+];
+
+document.body.onload = function(){
+  chrome.storage.sync.set({'data': TITLES});
+}

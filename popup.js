253f1725284dc0aa741352bc459c80c876834d35
Vinicius Ferreira Negrisolo <vinicius.negrisolo@gmail.com>
2016-09-05 10:08:12 -0400

    Add a jquery style method to find elements

--- popup.js
+++ popup.js
@@ -36,24 +36,34 @@ var TITLES = [
   "Weeds"
 ];
 
+function $(selector) {
+  var reg_id = /^#(.+)/;
+  var reg_class = /^\.(.+)/;
+  if(selector.match(reg_id)) {
+    return document.getElementById(selector.match(reg_id)[1]);
+  } else {
+    return document.getElementsByClassName(selector.match(reg_class)[1]);
+  }
+}
+
 document.addEventListener('DOMContentLoaded', function() {
   document.body.onload = function(){
     chrome.storage.sync.set({'data': TITLES});
 
     chrome.storage.sync.get('data', function(data){
-      document.getElementById('titles').innerHTML = "";
+      $('#titles').innerHTML = '';
 
       data.data.forEach(function(title){
         var node = document.createElement("li");
         var text = document.createTextNode(title);
         node.appendChild(text);
 
-        document.getElementById('titles').appendChild(node);
+        $('#titles').appendChild(node);
       });
     });
   }
 
-  document.getElementById('clear').onclick = function(){
+  $('#clear').onclick = function(){
     chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
       chrome.tabs.sendMessage(tabs[0].id, {'message': 'foo'});
     });
